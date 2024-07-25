4c34cff76c57690db1dfe3165b22aeb01f4a8309
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-07-25 04:01:58 +0300

    done

--- src/main/java/com/example/productservice/Product.java
+++ src/main/java/com/example/productservice/Product.java
@@ -1,2 +1,16 @@
-package com.example.productservice;public class Product {
+package com.example.productservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+    private Long id;
+    private String name;
+    private double price;
 }
